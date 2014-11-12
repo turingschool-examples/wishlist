ba6153c5b0f7e41418423a9ffd0ce74fec5b0695
Steve Kinney <hello@stevekinney.net>
2014-11-12 13:20:27 -0700

    More more adjustment to pending messages

--- spec/features/edit_with_spec.rb
+++ spec/features/edit_with_spec.rb
@@ -5,7 +5,7 @@ feature 'User edits an existing wish' do
   background :each do
     @wish = Wish.create(title: 'More snow', description: 'I enjoy scraping ice off my car.')
   end
-  
+
   scenario 'they fill in the form to update a wish' do
     visit edit_wish_path(@wish)
     fill_in 'Title', with: 'A great new idea!'
@@ -13,31 +13,31 @@ feature 'User edits an existing wish' do
     click_on 'Update Wish'
     expect(page).to have_css '.wish-title', 'A great new idea!'
   end
-  
+
   scenario 'they can omit a description' do
     visit new_wish_path
     fill_in 'Title', with: 'A great new idea!'
     click_on 'Create Wish'
     expect(page).to have_css '.wish-title', 'A great new idea!'
   end
-  
+
   scenario 'they can go back to viewing the wish' do
-    skip "You all are going to implement this."
+    skip "You are going to implement this."
   end
-  
+
   scenario 'they can go back the index of wishes' do
-    skip "You all are going to implement this."
+    skip "You are going to implement this."
   end
-  
+
   scenario 'it displays a success confirmation message' do
     success_message = 'Wish was successfully updated.'
-    skip "You all are going to implement this."
+    skip "You are going to implement this."
   end
-  
+
   # Test the unhappy path
-  
+
   scenario 'they get an error if they remove the title' do
-    skip "You all are going to implement this."
+    skip "You are going to implement this."
   end
-  
+
 end
--- spec/features/new_wish_spec.rb
+++ spec/features/new_wish_spec.rb
@@ -1,7 +1,7 @@
 require 'rails_helper'
 
 feature 'User creates a new wish' do
-  
+
   scenario 'they fill in the form to create a new wish' do
     visit new_wish_path
     fill_in 'Title', with: 'A great new idea!'
@@ -9,21 +9,21 @@ feature 'User creates a new wish' do
     click_on 'Create Wish'
     expect(page).to have_css '.wish-title', 'A great new idea!'
   end
-  
+
   scenario 'they can omit a description' do
     visit new_wish_path
     fill_in 'Title', with: 'A great new idea!'
     click_on 'Create Wish'
     expect(page).to have_css '.wish-title', 'A great new idea!'
   end
-  
+
   scenario 'it displays a success confirmation message' do
     success_message = 'Wish was successfully updated.'
-    skip "You all are going to implement this."
+    skip "You are going to implement this."
   end
-  
+
   # Test the unhappy path
-  
+
   scenario 'they get an error if they omit the title' do
     visit new_wish_path
     click_on 'Create Wish'
@@ -31,5 +31,5 @@ feature 'User creates a new wish' do
       expect(page).to have_content "Title can't be blank"
     end
   end
-  
+
 end
